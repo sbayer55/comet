276f33ee4062c11a4e7afb3e6b918802314e1d04
sbayer55 <sbayer55@gmail.com>
2015-09-24 23:41:46 -0400

    Moved ui to panel

--- client/controller/chatController.js
+++ client/controller/chatController.js
@@ -23,11 +23,32 @@ angular.module('comet').controller('chatController',
 
         $scope.removeAll = function() {
             $scope.chats.remove();
-        }
+        };
+
+        $scope.isCurrentUser = function(userName) {
+            if (typeof $rootScope.currentUser !== 'undefined')
+                if (typeof $rootScope.currentUser.emails !== 'undefined') {
+                    //console.log('This should be a user:');
+                    //console.log($rootScope.currentUser);
+                    return userName == $rootScope.currentUser.emails[0].address;
+                }
+            return false;
+        };
+
+        $scope.toConsole = function(value) {
+            console.log(value);
+        };
+
+        $scope.scroll = function() {
+            $('.conversation-panel').each(function(key, value) {
+                $(value).scrollTop(999999);
+            });
+        };
 
         // On Load:
 
         $scope.chats = $meteor.collection(Chats);
+
 
         console.log("chatController Done");
     }]);
--- client/lib/app.js
+++ client/lib/app.js
@@ -1,2 +1,17 @@
 console.log("Creating comet app.");
 angular.module('comet', ['angular-meteor', 'ui.router']);
+
+/*
+angular.module('comet').directive('ngScrollTo', function() {
+	return function(scope, element, attrs) {
+		angular.element(element).css('color', 'blue');
+		if (scope.$last) {
+			document.getElementsByClassName('.conversation-panel')[0].scrollTop = document.getElementsByClassName('.conversation-panel')[0].scrollHeight;
+			$('.conversation-panel').scrollTop($('.conversation-panel').scrollHeight);
+			console.log('Scroll height:');
+			console.log($('.conversation-panel').height);
+			//console.log($(element).html('Hello??????'));
+		}
+	};
+});
+	*/
